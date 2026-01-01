298cbc4705a7ee3455a96538f7e01c42ecb7d4a2
JAYDISH KENNEDY J <jaydishkennedy@gmail.com>
2026-01-01 23:34:53 +0530

    Create Contact.py

---
+++ pages/Contact.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(page_title="Contact – INSECTIFICA", page_icon="📞")
+
+st.title("👨‍🔬 Development Team")
+
+st.markdown("""
+**Department of Biotechnology**  
+St. Joseph’s College (Autonomous)  
+Tiruchirappalli – 620 002  
+
+📧 edward_bt2@mail.sjctni.edu  
+📧 cisgene.edward@gmail.com
+""")
+
+st.divider()
+
+st.markdown("""
+### 👨‍💻 App Concept & Development
+- Dr. A. Edward  
+- Dr. V. Swabna  
+- Dr. A. Asha Monica  
+- Dr. Pavulraj Michael SJ  
+
+### 🎓 Guidance
+Dr. Pavulraj Michael SJ  
+Rector, St. Joseph’s College (Autonomous)
+""")
